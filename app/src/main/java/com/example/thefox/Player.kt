c7e67c77aa1f97216f322cf53bbcb6a9bdb73011
Dapper314 <167818445+Dapper314@users.noreply.github.com>
2024-04-24 18:18:11 +0200

    Player.kt

--- app/src/main/java/com/example/thefox/Player.kt
+++ app/src/main/java/com/example/thefox/Player.kt
@@ -1,5 +1,11 @@
 package com.example.thefox
 
+import android.graphics.RectF
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.PointF
+import android.graphics.Color
+
 class Player {
     var color: Char = 'o'
     var SpeedX = 0F
@@ -7,5 +13,12 @@ class Player {
     var CaseWidth = 0F
     var CaseHeight = 0F
     var Life: Int = 10
-
+    var position = 0F
+    val playerColor = Paint()
+    init {
+        playerColor.color = Color.RED
+    }
+    fun draw(canvas: Canvas){
+        canvas.drawCircle(500f, 1000f, 100f, playerColor)
+    }
 }
