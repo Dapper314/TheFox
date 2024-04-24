c62a3250af4b4861d30d8ee160164acecf45895a
Dapper314 <167818445+Dapper314@users.noreply.github.com>
2024-04-24 19:58:19 +0200

    Update DrawingView.kt

--- app/src/main/java/com/example/thefox/DrawingView.kt
+++ app/src/main/java/com/example/thefox/DrawingView.kt
@@ -16,30 +16,45 @@ import android.view.SurfaceHolder
 import android.view.MotionEvent
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.FragmentActivity
+import java.util.*
 
 class DrawingView @JvmOverloads constructor (context: Context, attributes: AttributeSet? = null, defStyleAttr: Int = 0): SurfaceView(context, attributes,defStyleAttr) {
     lateinit var canvas: Canvas
-    val player = Player()
-    val platform = Platform()
-    val decor = Decor()
-    val obstacle = Obstacle()
-    val bonus = GameObject()
-    val thread = Thread()
-    val finishpoint = FinishPoint()
 
     val backgroundPaint = Paint()
-    init {
+    val player = Player()
+    var drawing = false
+
+    override fun draw(canvas: Canvas) {
+        super.draw(canvas)
         backgroundPaint.color = Color.WHITE
+        canvas.drawRect(0F, 0F, width.toFloat(), height.toFloat(),
+            backgroundPaint)
+        player.draw(canvas)
     }
-    fun draw() {
-        if (holder.surface.isValid) {
-            canvas = holder.lockCanvas()
-            canvas.drawRect(0f, 0f, 1000f,
-                2000f, backgroundPaint)
-            player.draw(canvas)
-            //obstacle.draw(canvas)
-            holder.unlockCanvasAndPost(canvas)
-        }
+    fun pause() {
+        drawing = false
     }
 
+    fun resume() {
+        drawing = true
+    }
+    fun run() {
+        var previousFrameTime = System.currentTimeMillis()
+
+        var currentTime = System.currentTimeMillis()
+        var elapsedTimeMS:Double=(currentTime-previousFrameTime).toDouble()
+        while (drawing) {
+            val currentTime = System.currentTimeMillis()
+            val elapsedTimeMS = (currentTime-previousFrameTime).toDouble()
+            updatePositions(elapsedTimeMS)
+            draw(canvas)
+            previousFrameTime = currentTime
+        }
+    }
+    fun updatePositions(elapsedTimeMS: Double) {
+        val interval = elapsedTimeMS / 1000.0
+        player.update(interval)
+        }
 }
+
